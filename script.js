05637f0f1a19d9eedeb16ea7d4abdf0a4d0d63de
TheSpiciestRamen <159206642+TheSpiciestRamen@users.noreply.github.com>
2024-03-07 10:33:47 +1100

    My first javascript

---
+++ script.js
@@ -0,0 +1,4 @@
+let PageHeadings = "Hello fellow conservative ";
+let EnterName = prompt("Please enter your name with a number");
+let ExclamationMark = "!"
+alert(PageHeadings + EnterName + ExclamationMark);
