715d7115fd04b6233dd58ccb6a900659d9d8f617
Ben Jones <benpaddlejones@outlook.com>
2024-03-13 14:49:22 +1100

    add missed lesson

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let PageHeadings = "Hello fellow conservative ";
-let EnterName = prompt("Please enter your name with a number");
-let ExclamationMark = "!"
-alert(PageHeadings + EnterName + ExclamationMark);
+let enterName = prompt("Enter your name");
+let playerGuess;
+let computerGuess = randomNum();
+let playerFeedback = computerGuess;
+loadDIV();
+
+function randomNum() {  
+  return Math.floor(Math.random() * (10 - 1 + 1) + 1)
+}
+
+function loadDIV () {
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Guess a number between 1 & 10</H2>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback
+}
+
+function enterNumber () {
+    playerGuess = prompt("Enter a number") 
+}
